fc38f0efbedba57ba922b5d1a7351281743cb53e
Mark <bm.work.free@gmail.com>
2021-04-22 20:17:31 +0300

    Add factory for Post model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(255),
+            'description' => $this->faker->realText(1000),
+            'date' => now(),
+            'user_id' => function () {
+                return User::first()->id;
+            },
+        ];
+    }
+}
