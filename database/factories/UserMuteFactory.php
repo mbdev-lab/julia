1ffc975c8dfd410730e151d435c6c681ff0d854b
Mark <bm.work.free@gmail.com>
2021-04-23 21:51:18 +0300

    Fix userMute model
    remove method

--- database/factories/UserMuteFactory.php
+++ database/factories/UserMuteFactory.php
@@ -28,17 +28,4 @@ class UserMuteFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
 }
