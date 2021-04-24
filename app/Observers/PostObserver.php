cffeb5edabe99326acb87b0517b6701f1caab50b
Mark <bm.work.free@gmail.com>
2021-04-25 00:01:42 +0300

    Make Post observer
    need for cache invalidation

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
+
+class PostObserver
+{
+    /**
+     * Handle the Post "created" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function created(Post $post)
+    {
+        $cacheKey = 'post_' . md5($post->user_id);
+        $cached = Cache::get($cacheKey);
+
+        if(!empty($cached) && $cached instanceof Collection) {
+            $cached->push($post);
+            Cache::put($cacheKey, $cached, Carbon::now()->addMinutes(60));
+        }
+    }
+
+    /**
+     * Handle the Post "updated" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function updated(Post $post)
+    {
+        //...
+    }
+
+    /**
+     * Handle the Post "deleted" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function deleted(Post $post)
+    {
+        //...
+    }
+
+    /**
+     * Handle the Post "restored" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function restored(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "force deleted" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function forceDeleted(Post $post)
+    {
+        //
+    }
+}
